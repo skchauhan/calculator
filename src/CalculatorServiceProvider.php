6d97203fa9ce87393485f3034620e123451eb751
skchauhan <skrajput42@gmail.com>
2017-12-22 16:24:42 +0530

    Add files via upload

---
+++ src/CalculatorServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace devdojo\calculator;
+
+use Illuminate\Support\ServiceProvider;
+
+class CalculatorServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        include __DIR__.'/routes.php';
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+}
